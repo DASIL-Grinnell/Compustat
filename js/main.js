1af93792b62b930807cfd7974ed940d717943061
Alex Mitchell <alex@alexthemitchell.com>
2016-09-29 22:09:28 +0000

    Use date for x axis

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ function plotTracesInDiv(divID, traces) {
     title: GRAPH_TITLE,
     xaxis: {
       title: XAXIS_LABEL,
+      type: 'date'
     },
     yaxis: {
       title: YAXIS_LABEL,
@@ -51,7 +52,7 @@ function companyFromRow(row) {
   let company = {
       name: obj.Company,
       data: {
-        x: [1998 , 1999, 2000, 2001, 2002,2003, 2004, 2005, 2006,2007,2008,2009,2009,2011,2012,2013],
+        x: ['1998-01-01' , '1999-01-01', '2000-01-01', '2001-01-01', '2002-01-01','2003-01-01', '2004-01-01', '2005-01-01', '2006-01-01','2007-01-01','2008-01-01','2009-01-01','2010-01-01','2011-01-01','2012-01-01','2013-01-01'],
         y: [obj["1998"], obj["1999"], obj["2000"], obj["2001"], obj["2002"], obj["2003"], obj["2004"], obj["2005"], obj["2006"], obj["2007"], obj["2008"], obj["2009"], obj["2010"], obj["2011"], obj["2012"], obj["2013"]]
       },
   }
@@ -62,7 +63,7 @@ function companyFromRow(row) {
 function main() {
   var complete = function() {console.log("done");};
   fetchCompanies(companyFromRow, function() {
-  plotCompaniesInDiv("myDiv" , companies);
+    plotCompaniesInDiv("myDiv" , companies.slice(1,5));
   });
 }
 
