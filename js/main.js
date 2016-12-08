a0d76a165a65069428b7422d96203193b10a718f
Alex Mitchell <alex@alexthemitchell.com>
2016-12-08 00:06:17 -0600

    Finish CSV parsing

--- js/main.js
+++ js/main.js
@@ -8,35 +8,32 @@ var displayedTraces = [];
 var sectors = {
   data: {},
   insertCompany: function(company) {
+    if (this.data[company.sector] == undefined) {
+      this.data[company.sector] = {}
+    }
+    if (this.data[company.sector][company.industry] == undefined) {
+      this.data[company.sector][company.industry] = {}
+    }
     this.data[company.sector][company.industry][company.name] = {
+      name: company.name,
       x: ['2000-01-01',    '2001-01-01',   '2002-01-01',  '2003-01-01',  '2004-01-01',   '2005-01-01', '2006-01-01'],
       y: company.datapoints
     }
   },
   getSectors: function(){
-    return this.data.map(function(datum) {
-      return datum.sector;
-    });
+     return Object.keys(this.data).filter(function(x){
+       return sectors.data.hasOwnProperty(x)
+     })
   },
   getIndustries: function(sector) {
-    return flatten(this.data.map(function(datum) {
-      if (sector == null || datum.sector == sector){
-        return datum.industries.map(function(indust){
-          return indust.industry
-        });
-      }
-    }));
+    return Object.keys(this.data[sector]).filter(function(x) {
+      return sectors.data[sector].hasOwnProperty(x)
+    })
   },
-  getCompanies: function(industry) {
-    return flatten(this.data.map(function(sectors) {
-      return flatten (sectors.industries.map(function(indust) {
-        if (industry == null || industry == indust.industry) {
-          return indust.companies.map(function(company) {
-            return company.company
-          });
-        };
-      }));
-    }));
+  getCompanies: function(sector, industry) {
+    return Object.keys(this.data[sector][industry]).filter(function (x) {
+      return sectors.data[sector][industry].hasOwnProperty(x)
+    })
   }
 }
 
@@ -45,10 +42,10 @@ var sectors = {
 function traceForCompany(company) {
   var trace = {};
 
-  trace.x = company.data.x;
-  trace.y = company.data.y;
+  trace.x = company.x;
+  trace.y = company.y;
   trace.type = 'scatter';
-  trace.name = company.company;
+  trace.name = company.name;
   console.log(trace)
   return trace;
 }
@@ -83,27 +80,12 @@ function fetchCompanies(complete) {
       company.datapoints = [d['2000'],d['2001'],d['2002'],d['2003'],d['2004'],d['2005'],d['2006'],d['2007'],d['2008'],d['2009'],d['2010'],d['2011'],d['2012'],d['2013'],d['2014']]
       return company
     });
-    for (company in companies) {
-     sectors.insertCompany(company) 
+    for (var i = 0; i < companies.length; i++) {
+      sectors.insertCompany(companies[i]) 
     }
     if (complete) { complete() }
   });
 }
-
-function companyFromRow(row) {
-  let obj = row.data[0];
-  let industry = obj.Industry
-  let company = obj.Company
-  let cpny = {
-      name: obj.Company,
-      data: {
-        x: ['1998-01-01' , '1999-01-01', '2000-01-01', '2001-01-01', '2002-01-01','2003-01-01', '2004-01-01', '2005-01-01', '2006-01-01','2007-01-01','2008-01-01','2009-01-01','2010-01-01','2011-01-01','2012-01-01','2013-01-01'],
-        y: [obj["1998"], obj["1999"], obj["2000"], obj["2001"], obj["2002"], obj["2003"], obj["2004"], obj["2005"], obj["2006"], obj["2007"], obj["2008"], obj["2009"], obj["2010"], obj["2011"], obj["2012"], obj["2013"]]
-      },
-  }
-  companies.push(cpny);
-}
-
 
 function flatten(ary) {
       var ret = [];
@@ -133,9 +115,9 @@ function didSelectSector(select) {
 
 function didSelectIndustry(select) {
   var companySel = document.getElementById("companySelector")
+  var sectorSel = document.getElementById("sectorSelector")
   clearCompanyDropdown();
-  console.log(select.value)
-  var companies = sectors.getCompanies(select.value)
+  var companies = sectors.getCompanies(sectorSel.value, select.value)
   for (company in companies) {
     var opt = document.createElement("option");
     opt.value = companies[company];
@@ -169,21 +151,9 @@ function addLine() {
   var sectorName = document.getElementById("sectorSelector").value
   var industryName = document.getElementById("industrySelector").value
   var companyName = document.getElementById("companySelector").value
-  for (sector in sectors.data) {
-    if (sectors.data[sector].sector == sectorName) {
-      for (industry in sectors.data[sector].industries) {
-        if (sectors.data[sector].industries[industry].industry == industryName) {
-          for (company in sectors.data[sector].industries[industry].companies) {
-            if (sectors.data[sector].industries[industry].companies[company].company == companyName) {
-              console.log("Found company");
-              displayedTraces.push(traceForCompany(sectors.data[sector].industries[industry].companies[company]))
-              plotTracesInDiv("myDiv", displayedTraces);
-            }
-          }
-        }
-      }
-    }
-  }
+  let company = sectors.data[sectorName][industryName][companyName]
+  displayedTraces.push(traceForCompany(company))
+  plotTracesInDiv("myDiv", displayedTraces);
 }
 
 plotTracesInDiv("myDiv",displayedTraces);
